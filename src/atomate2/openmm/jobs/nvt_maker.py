d74a500e95e8c06597fc579d935f68157920c74b
orionarcher <orion@lbl.gov>
2023-05-10 15:57:43 -0700

    remove thermostat from nvt maker

--- src/atomate2/openmm/jobs/nvt_maker.py
+++ src/atomate2/openmm/jobs/nvt_maker.py
@@ -10,34 +10,20 @@ class NVTMaker(BaseOpenMMMaker):
     steps: int = 1000000
     name: str = "nvt simulation"
     temperature: float = 298
-    frequency: int = 10
 
     def _run_openmm(self, sim):
-        context = sim.context
-        system = context.getSystem()
-        assert (
-            system.usesPeriodicBoundaryConditions()
-        ), "system must use periodic boundary conditions for pressure equilibration."
-        thermostat_force_index = system.addForce(
-            AndersenThermostat(self.temperature * kelvin, pico * second * self.frequency)
-        )
-
-        # Re-init the context afte adding thermostat to System
-        context.reinitialize(preserveState=True)
+        integrator = sim.context.getIntegrator()
+        integrator.setTemperature(self.temperature * kelvin)
 
         # Run the simulation
         sim.step(self.steps)
-
-        # Remove thermostat and update context
-        system.removeForce(thermostat_force_index)
-        context.reinitialize(preserveState=True)
 
         task_details = TaskDetails(
             task_name=self.name,
             task_kwargs={
                 "steps": self.steps,
                 "temperature": self.temperature,
-                "frequency": self.frequency,
+                # "frequency": self.frequency,
             },
             platform_kwargs=self.platform_kwargs,
         )
